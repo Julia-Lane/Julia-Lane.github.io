455170553fe0937a15a2d78c4b7a0430b78ea4d0
Julia Lane <lanejm99@gmail.com>
2020-10-04 13:28:46 +1100

    holiday changes

--- Fleet/assets/js/main.js
+++ Fleet/assets/js/main.js
@@ -14,21 +14,16 @@ $(document).ready(function(){
 	$('.btn-anonymous').click(function(){
 		$('.item').addClass('hide');
 		$('.item.anonymous').removeClass('hide');
-	});
+	});	
 
 	$('.btn-occasion').click(function(){
 		$('.item').addClass('hide');
 		$('.item.occasion').removeClass('hide');
 	});
 
-	$('.btn-home').click(function(){
+
+	$('.btn-reset').click(function(){
 		$('.item').removeClass('hide');
-	});
-
-	$('.square').click(function(){
-		$(this).toggleClass('move');
-	});
-
-
+	});	
 
 });
---
+++ Folio/assets/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	console.log('hello world');
+
+
+	// 	$('.btn-menu').click(function(){
+	// 	$('.inspo').toggleClass('hide');
+	// 	$('.inspo.menu').toggleClass('hide');
+	// });
+
+
+});
